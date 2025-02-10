9f5d9e90217d1e09da909586dea4f1a03adb01f7
Hanna-Hinn <hanna.hinn30@gmail.com>
2025-02-10 19:58:07 +0200

    backend

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+// const dev = process.env.NODE_ENV !== "production";
+
+export const server = "http://localhost:3000";
+
+export const BACKEND_URL =
+  "https://green-sage-backend-f0f077d7fa9c.herokuapp.com/api";
+
+export const SOCKET_URL =
+  "https://green-sage-backend-f0f077d7fa9c.herokuapp.com";

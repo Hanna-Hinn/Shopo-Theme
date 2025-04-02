c0c5b1f4a1abe8a47abdfb9e6c541c30e74f9ecb
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2025-04-02 20:45:03 +0300

    Update src/api/endpoints.js

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,6 +2,6 @@ import { BACKEND_URL } from "../config";
 
 export const endpoints = {
   login: `${BACKEND_URL}/login`,
-  getUser: `${BACKEND_URL}/users`,
+  users: `${BACKEND_URL}/users`,
   updateUser: `${BACKEND_URL}/users`,
 };
